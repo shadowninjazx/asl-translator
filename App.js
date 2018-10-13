b22e15dda03126d01b511e29eda5c7d756d70ba0
TashaGospel <nhanhnhanh17399@gmail.com>
2018-10-13 07:15:08 +0100

    Placeholder text for camera

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: "HELLO WORLD"};
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.cameraView}>
+          <Text>This is camera</Text>
+        </View>
+        <View style={styles.textBox}>
+          <Text style={styles.text}>{this.state.text}</Text>
+        </View>
       </View>
     );
   }
@@ -13,9 +23,24 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+      flex: 1,
+  },
+
+  cameraView: {
+    flex: 2,
+    backgroundColor: '#35ebff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textBox: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 30,
+  }
 });
